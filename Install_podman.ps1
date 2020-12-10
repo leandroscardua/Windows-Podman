eefb20e009444fd86e2c62d68c0a7a15e10446d0
Leandro Scardua <leandroscar@gmail.com>
2020-12-10 16:49:02 -0500

    Update Install_podman.ps1

--- Install_podman.ps1
+++ Install_podman.ps1
@@ -1,10 +1,20 @@
+
 $source="https://github.com/containers/podman/releases/latest/download/podman-remote-release-windows.zip"
 $destination="$env:HOMEDRIVE\podman"
 
-mkdir -force $destination
+Write-Host "Creating folder on $destination"
+mkdir -force $destination | Out-Null
 
-curl.exe $source -L -o "$destination\podman.zip"
+Write-Host "Downloading podman to $destination"
+curl.exe --silent $source -L -o "$destination\podman.zip"
 
+Write-Host "Expand podman file to $destination"
 Expand-Archive -LiteralPath "$destination\podman.zip" -DestinationPath $destination -Force
 
+Write-Host "Setting $destination to System Environment Variable"
+$env:Path += ";$destination" 
+[Environment]::SetEnvironmentVariable("Path", $env:Path, [System.EnvironmentVariableTarget]::Machine)
+
+
+Write-Host "Cleaning after the installation"
 Remove-Item -LiteralPath "$destination\podman.zip"
